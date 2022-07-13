7164fc4d93db81f019a4491559c699c64e1b81c3
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-13 16:20:44 +0100

    components layout of avoid repetition

---
+++ Components/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+const Layout = ({ children }: any) => {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default Layout;
