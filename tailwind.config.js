a32373a1e9166e2dd4ecf979d791563e6251806e
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-13 16:19:31 +0100

    tailwinsConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
