03e523ce0b5198029e0be7f8d75dc033dae3d53d
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-13 17:25:33 +0100

    fetch data de graphql

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import { PostCard, Categories, POstWindget } from "../Components";
 import Head from "next/head";
-const index = () => {
-  const posts = [
-    { title: "React Testing", excerpt: "learn React TEsting" },
-    { title: "React with tailxwind", excerpt: "learn React with tailwind" },
-  ];
+import { getPosts } from "../Services/index";
+const index = ({ posts }:any) => {
+  // console.log(posts);
+  
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -12,23 +11,27 @@ const index = () => {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: any, index) => {
-            return (
-              <div>
-                <PostCard post={post} key={post.title} />
-              </div>
-            );
-          })}
+          {posts?.map((post: any) => (
+          <PostCard key={post.title}  post={post} />
+          ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <POstWindget/>
-            <Categories/>
+            <POstWindget />
+            <Categories />
           </div>
         </div>
       </div>
     </div>
   );
 };
+
+export async function getStatitProps() {
+  const posts = (await getPosts()) || [];
+  console.log(posts);
+  return {
+    props: { posts },
+  };
+}
 
 export default index;
