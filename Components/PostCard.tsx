b00f162b54464c4a0b8266d20d3c8e1533b7c58f
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-13 17:24:56 +0100

    card de post

--- Components/PostCard.tsx
+++ Components/PostCard.tsx
@@ -1,11 +1,18 @@
-
-const PostCard = ({post}:any) => {
+import Link from "next/link";
+const PostCard = ({ post }: any) => {
+  console.log(post);
+  console.log("kfmgjmfdljlgmfd");
   return (
-    <>
-      <div>{post.title}</div>
-      <div>{post.excerpt}</div>
-    </>
+    <div className="bg-white shadow-lg rounded-lg p-0 lg-p-8 pb-12 mb-8">
+      <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+        <img
+          src={post.featuredImage.url}
+          alt={post.title}
+          className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg "
+        />
+      </div>
+    </div>
   );
-}
+};
 
-export default PostCard
+export default PostCard;
