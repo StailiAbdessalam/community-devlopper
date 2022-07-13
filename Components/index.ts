0e1a9a2917c93167ff697f87e2028436c0f0c9c4
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-13 16:19:59 +0100

    export header components

--- Components/index.ts
+++ Components/index.ts
@@ -1,3 +1,4 @@
-export {default as PostCard} from './PostCard';
-export {default as Categories} from './Categories';
-export {default as POstWindget} from './PostWidget';
+export { default as PostCard } from './PostCard';
+export { default as Categories } from './Categories';
+export { default as POstWindget } from './PostWidget';
+export { default as Header } from './Header';
