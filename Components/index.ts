06c9e7f2837a981334c49d4b5485915407ad2a91
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-14 17:47:32 +0100

    exportation de compoents

--- Components/index.ts
+++ Components/index.ts
@@ -2,3 +2,7 @@ export { default as PostCard } from './PostCard';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
 export { default as Header } from './Header';
+export { default as Author } from './Author';
+export { default as Comments } from './Comments';
+export { default as CommentsForm } from './CommentsForm';
+export { default as Postdetails } from './PostDetails';
