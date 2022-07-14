7b0955b83271bd708d26809f01dd99ae49daa911
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-14 17:47:20 +0100

    modifie header alyout

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,23 +1,28 @@
 import React, { useContext } from "react";
 import Link from "next/link";
-
+import { useState,useEffect } from "react";
+import { getCategories } from "../Services";
 const Header = () => {
-  const categories = [
-    { name: "React", slug: "react" },
-    { name: "Web Devloppement", slug: "web-dev"},
-  ];
+  const [categories, setCategories]: any = useState([]);
+  useEffect(() => {
+    getCategories().then((res) => {
+      setCategories(res);
+    });
+  }, []);
+
+
   return (
     <div className="container mx-auto px-10 mb-8">
       <div className="border-b w-full inline-block border-blue-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              SPORTime
+              Benjy Dev
             </span>
           </Link>
         </div>
         <div className="hidden md:float-left md:contents">
-            {categories.map((category)=>(
+            {categories.map((category:any)=>(
                 <Link key={category.slug} href={`/category/${category.slug}`}>
                     <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                     {category.name}
