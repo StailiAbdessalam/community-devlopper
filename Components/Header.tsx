74c5e707e679095387978546f085586cd1ec4c3f
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-14 16:52:05 +0100

    POst card

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Graphcms
+              SPORTime
             </span>
           </Link>
         </div>
