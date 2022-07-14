b5209d3102421f2540e361ee989e27893079b232
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-14 16:51:50 +0100

    services graphql for consommer graphcms

--- Services/index.ts
+++ Services/index.ts
@@ -1,32 +1,33 @@
 import { request, gql } from 'graphql-request'
 
 const graphqlAPI: any = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
 export const getPosts = async () => {
     const query = gql`
        query MyQuery {
             postsConnection {
                 edges {
-                node {
-                    author {
-                    bio
-                    name
-                    id
-                    photo {
+                    node {
+                        author {
+                            bio
+                            name
+                            id
+                            photo {
+                                url
+                            }
+                        }
+                        createdAt
+                        slug
+                        title
+                        exercpt 
+                        featuredImages {
                         url
+                        }
+                        categories {
+                        slug
+                        name
+                        }
                     }
-                    }
-                    createdAt
-                    slug
-                    title
-                    exercpt
-                    featuredImages {
-                    url
-                    }
-                    categories {
-                    slug
-                    name
-                    }
-                }
                 }
             }
             }
@@ -34,3 +35,45 @@ export const getPosts = async () => {
     const results = await request(graphqlAPI, query);
     return results.postsConnection.edges;
 }
+
+
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetPostDetails {
+         posts(orderBy: createdAt_ASC, last: 3) {
+          title
+          featuredImages {
+             url
+          createdAt
+          }
+          slug
+  }
+    }
+  `;
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+};
+
+
+export const getSimilarPosts = async (categories: any, slug: any) => {
+    const query = gql`
+      query GetPostDetails($slug: String!, $categories: [String!]) {
+        posts(
+          where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+          last: 3
+        ) {
+          title
+          featuredImages {
+            url
+          }
+          createdAt
+          slug
+        }
+      }
+    `;
+    const result = await request(graphqlAPI, query, { slug, categories });
+
+    return result.posts;
+};
