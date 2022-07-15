dfd9f0896ab661fc4f6f41bda0aa16563d92e55c
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-15 17:37:46 +0100

    exportation the FeaturedPostCard

--- Components/index.ts
+++ Components/index.ts
@@ -6,3 +6,4 @@ export { default as Author } from './Author';
 export { default as Comments } from './Comments';
 export { default as CommentsForm } from './CommentsForm';
 export { default as PostDetail } from './PostDetail';
+export { default as FeaturedPostCard } from './FeaturedPostCard';
