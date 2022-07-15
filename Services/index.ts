865f576a86c4a49c5b18fa381be7d912f37a109b
BENJY33X <A.STAILI@student.youcode.ma>
2022-07-15 15:44:45 +0100

    ad function in index file to check submit form

--- Services/index.ts
+++ Services/index.ts
@@ -132,3 +132,15 @@ export const getPostDetails = async (slug: any) => {
   return result.post;
 };
 
+
+export const submitComment = async (obj:any) => {
+  const result = await fetch('/api/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(obj), 
+  });
+
+  return result.json();
+};
